71b00d4e2838abe6c98edd031980ab3bf2db0764
admin-automatify <admin@automatify.com.au>
2026-09-10 20:44:26 +1000

    feat: add Odoo 19 addon manifest

---
+++ knowledge_markdown/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Knowledge Markdown",
+    "version": "19.0.1.0.0",
+    "summary": "Import and export OCA Knowledge pages as Markdown",
+    "category": "Knowledge",
+    "author": "Automatify Pty Ltd",
+    "website": "https://github.com/Automatify-Pty-Ltd/odoo-addons",
+    "license": "AGPL-3",
+    "depends": ["document_page"],
+    "external_dependencies": {"python": ["markdown"]},
+    "data": [
+        "security/ir.model.access.csv",
+        "views/markdown_import_wizard_views.xml",
+        "views/document_page_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
